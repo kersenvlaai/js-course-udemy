40d7c859e035fb4dc000204487352768a9f987d2
kersenvlaai <sonnyluu@gmail.com>
2022-07-05 23:13:14 +0200

    added function to open modal and close modal

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,7 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsShowModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
