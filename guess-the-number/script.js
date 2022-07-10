18c5a4dbfa3f61002a91bf671b91e0ea53116525
kersenvlaai <sonnyluu@gmail.com>
2022-07-10 23:18:48 +0200

    delete unused

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const secretNumberGenerator = () => Math.floor(Math.random() * 20) + 1;
 
@@ -15,50 +15,47 @@ let highscore = 0;
 function checkHighscore(score) {
   if (score > highscore) {
     highscore = score;
-    document.querySelector('.highscore').textContent = highscore;
+    document.querySelector(".highscore").textContent = highscore;
   }
 }
 
-document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.number').style.width = '15rem';
-  changeTextContent('.number', '?');
-  changeValue('.guess', '');
-  document.querySelector('body').style.backgroundColor = '#222';
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".number").style.width = "15rem";
+  changeTextContent(".number", "?");
+  changeValue(".guess", "");
+  document.querySelector("body").style.backgroundColor = "#222";
   secretNumber = secretNumberGenerator();
   score = 20;
-  changeTextContent('.score', score);
-  changeTextContent('.message', 'Start guessing!');
+  changeTextContent(".score", score);
+  changeTextContent(".message", "Start guessing!");
 });
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    document.querySelector(".message").textContent = "No number!";
   } else if (guess === secretNumber) {
-    changeTextContent('.message', 'Correct number!');
-    document.querySelector('body').style.backgroundColor = 'green';
-    document.querySelector('.number').style.width = '30rem';
-    changeTextContent('.number', secretNumber);
+    changeTextContent(".message", "Correct number!");
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".number").style.width = "30rem";
+    changeTextContent(".number", secretNumber);
     checkHighscore(score);
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
+      document.querySelector(".message").textContent =
         guess > secretNumber
-          ? 'Incorrect number! Guess lower...'
-          : 'Incorrect number! Guess higher...';
+          ? "Incorrect number! Guess lower..."
+          : "Incorrect number! Guess higher...";
       score--;
-      changeTextContent('.score', score);
+      changeTextContent(".score", score);
     } else {
       changeTextContent(
-        '.message',
-        'You lost! Correct number was ' + secretNumber
+        ".message",
+        "You lost! Correct number was " + secretNumber
       );
-      changeTextContent('.score');
+      changeTextContent(".score");
     }
   } else {
   }
 });
-
-
-export function getCurrentMonth = msg => 'nl',
